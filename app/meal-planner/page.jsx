9194bb9b19c80db4bd0e492b34247ff3f72a8be4
multibillionaire202505-arch <multibillionaire202505@gmail.com>
2025-09-29 21:27:56 -0500

    Create page.jsx

---
+++ app/meal-planner/page.jsx
@@ -0,0 +1,8 @@
+export default function MealPlanner() {
+  return (
+    <div style={{padding:20}}>
+      <h1>Meal Planner ✅</h1>
+      <p>If you see this, the route works.</p>
+    </div>
+  );
+}
